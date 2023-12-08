491f17c0da769eef825b1e11a0b7f224d9b49c00
Tnhnyldz <tunahanyildiz@outlook.it>
2023-12-08 23:34:05 +0300

    axios

--- src/services/api.js
+++ src/services/api.js
@@ -12,3 +12,20 @@ const instance = axios.create({
 });
 
 export default instance;
+
+// import axios from "axios";
+
+// const config = {
+//   headers: {
+//     "Access-Control-Allow-Origin": "*",
+//     "Content-Type": "text/plain",
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNDI2Njc3NzY0YWI0NjcwNjFkODRhZGYxZmM1M2Y0MCIsInN1YiI6IjY0ZDIyMDc3NGQ2NzkxMDBjNTJkYjAxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._88bsgtEaFC1687FqOijtXPYwekDQPxE97cCqKvPlVs'
+//   },
+// };
+
+// const instance = axios.create({
+//   baseURL: "https://api.themoviedb.org/3/",
+//   https: config,
+// });
+
+// export default instance;
